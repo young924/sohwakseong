14a193437e7143f1c976e52593d29a2c7b9a6bb1
HyoRyoung-Yang <js980303@naver.com>
2022-01-12 00:08:38 +0900

    기능: Router.js 생성

---
+++ frontend/src/Router.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
