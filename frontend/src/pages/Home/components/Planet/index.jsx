9fc5e0f0dc1d4d9b4c5d9400587c779ea9dd128c
HyoRyoung-Yang <js980303@naver.com>
2022-01-14 01:21:08 +0900

    기타: 더블클릭 페이지 이동 로직 추가

--- frontend/src/pages/Home/components/Planet/index.jsx
+++ frontend/src/pages/Home/components/Planet/index.jsx
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import princeImg from "../../../../assets/image/prince.png";
 import foxImg from "../../../../assets/image/fox.png";
 import starImg from "../../../../assets/image/star.png";
 import planetImg from "../../../../assets/image/planet.png";
 import * as S from "./style";
+import { useHistory } from "react-router-dom";
 
 function Planet() {
   const [clickedIndex, setClickedIndex] = useState(0);
+  const history = useHistory();
+  const handleClick = useCallback(
+    (index) => {
+      console.log("클릭");
+      if (index !== clickedIndex) {
+        setClickedIndex(() => index);
+        return;
+      }
+      switch (index) {
+        case 1:
+          history.push("/market");
+          break;
+        case 2:
+          history.push("/mypage");
+          break;
+        case 3:
+          history.push("/achievecheck");
+          break;
+        default:
+          break;
+      }
+    },
+    [clickedIndex]
+  );
+
   return (
     <S.Container clickedIndex={clickedIndex}>
-      <S.Star src={starImg} onClick={() => setClickedIndex(1)} />
-      <S.Prince src={princeImg} onClick={() => setClickedIndex(2)} />
-      <S.Fox src={foxImg} onClick={() => setClickedIndex(3)} />
+      <S.Star src={starImg} onClick={() => handleClick(1)} />
+      <S.Prince src={princeImg} onClick={() => handleClick(2)} />
+      <S.Fox src={foxImg} onClick={() => handleClick(3)} />
       <S.Planet src={planetImg} />
     </S.Container>
   );
