ebcd09d0b0fb157fce3c78d9e91b8e7c98de6e3e
HyoRyoung-Yang <js980303@naver.com>
2022-01-15 02:02:41 +0900

    기능: 로딩 페이지 완성

--- frontend/src/pages/Home/components/Loading/style.js
+++ frontend/src/pages/Home/components/Loading/style.js
@@ -7,6 +7,11 @@ export const Container = styled.div`
   background-color: black;
   color: white;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+  > img {
+    width: 9.3rem;
+    height: 6rem;
+  }
 `;
