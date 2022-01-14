f22c721bf8a76b080f70d947a6e49f3e7fff91f3
HyoRyoung-Yang <js980303@naver.com>
2022-01-15 00:07:25 +0900

    기능: 마이페이지 tab, notice ui 작성

--- frontend/src/pages/MyPage/components/BottomModal/index.jsx
+++ frontend/src/pages/MyPage/components/BottomModal/index.jsx
@@ -1,13 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import useMount from "../../../../hooks/useMount";
+
 import * as S from "./style";
 
 function BottomModal() {
-  const [isOpen, setIsOpen] = useState(false);
+  const isMount = useMount();
+  const [isComplete, setIsComplete] = useState(false);
+  const [isNoticeOn, setIsNoticeOn] = useState(true);
 
   useEffect(() => {
-    setIsOpen(true);
+    setTimeout(() => {
+      setIsNoticeOn(false);
+    }, 5000);
   }, []);
-  return <S.Container isOpen={isOpen}></S.Container>;
+
+  return (
+    <S.Container isMount={isMount}>
+      <S.TabWrapper>
+        <S.Tab isComplete={!isComplete} onClick={() => setIsComplete(false)}>
+          진행 중
+        </S.Tab>
+        <S.Tab isComplete={isComplete} onClick={() => setIsComplete(true)}>
+          진행 완료
+        </S.Tab>
+      </S.TabWrapper>
+      <S.Notice isNoticeOn={isNoticeOn}>
+        *소확성을 꾸준히 달성하면 행성에 누군가가 놀러옵니다~!
+      </S.Notice>
+    </S.Container>
+  );
 }
 
 export default BottomModal;
--- frontend/src/pages/MyPage/components/BottomModal/style.js
+++ frontend/src/pages/MyPage/components/BottomModal/style.js
@@ -8,10 +8,57 @@ export const Container = styled.section`
   opacity: 0;
   transition: all 0.45s ease-in-out;
 
-  ${({ isOpen }) =>
-    isOpen &&
+  ${({ isMount }) =>
+    isMount &&
     css`
       height: 70vh;
-      opacity: 0.7;
+      opacity: 0.85;
     `}
+`;
+
+export const TabWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.2);
+`;
+
+export const Tab = styled.div`
+  opacity: 0.7;
+  width: 30%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-weight: 500;
+  font-size: 1.8rem;
+  padding: 2.4rem;
+  padding-bottom: 1.4rem;
+  cursor: pointer;
+
+  ${({ isComplete }) =>
+    isComplete &&
+    css`
+      border-bottom: 2px solid #ffff;
+      font-weight: 700;
+      opacity: 1;
+    `}
+`;
+
+export const Notice = styled.p`
+  position: absolute;
+  bottom: 1.2rem;
+  width: 100%;
+  margin: 0;
+  text-align: center;
+  font-size: 1.4rem;
+  color: #ffd57c;
+  opacity: 1;
+  transition: all 0.5s linear;
+  ${({ isNoticeOn }) =>
+    !isNoticeOn &&
+    css`
+      opacity: 0;
+    `};
 `;
