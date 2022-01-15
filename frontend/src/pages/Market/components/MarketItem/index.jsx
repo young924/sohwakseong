f3e165988dfaaa78c46d9231dbe8da7586a7cc22
hanyoung <phanyoung@naver.com>
2022-01-15 17:28:15 +0900

    searchbar

---
+++ frontend/src/pages/Market/components/MarketItem/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import * as S from './style'
+
+function MarketItem() {
+  return (
+  <>
+    <S.Container>
+      <S.EmoticonContainer>
+        <S.Emoticon>
+          ⭐️
+        </S.Emoticon>
+      </S.EmoticonContainer>
+    </S.Container>
+  </>
+  );
+}
+
+export default MarketItem
---
+++ frontend/src/pages/Market/components/MarketItem/style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: rgba(255, 255, 255, 0.8);;
+  width: 35rem;
+  height: 8rem;
+  border-radius: 1.2rem;
+  display: flex;
+  align-items: center;
+  padding-left: 1.5rem;
+`
+
+export const EmoticonContainer = styled.div`
+  background-color: rgba(255, 255, 255, 1);;
+  width: 5rem;
+  height: 5rem;
+  border-radius: 3rem;
+`
+
+export const Emoticon = styled.p`
+  font-size: 10px;
+`
